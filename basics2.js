63a98ec9fdd366b5b1b4bdd4ae0dcabb8c11a6af
Divyanshu <140911251+learneroflife30@users.noreply.github.com>
2023-07-31 01:15:05 +0530

    Add files via upload

--- basics2.js
+++ basics2.js
@@ -4,6 +4,8 @@ const winters = true;
 
 // if else loop runs for only one time - https://www.w3schools.com/js/js_if_else.asp
 
+console.log("*** if, else if, else output below ***");
+
 if (summers) {
   console.log("Its summer season in India");
 } else if (!autumn) {
@@ -14,9 +16,7 @@ if (summers) {
   console.log("Its a mixed weather season in India");
 }
 
-// While, do while loops
-//https://www.w3schools.com/jsref/jsref_dowhile.asp
-
+console.log("*** While loop output below ***");
 // We use while loop when we need a piece of code to keep on running till a condition is satisfied
 let i = 0;
 while (i < 10) {
@@ -24,9 +24,58 @@ while (i < 10) {
   console.log("The number is " + i);
 }
 
+let statement = true;
+while (statement) {
+  console.log(statement);
+  statement = false;
+}
+
+console.log("*** Do While loop output below ***");
 // We use do while loop when we want a piece of code to run atleast once before checking if a condition is satisfied or not
 let k = 0;
 do {
   k++;
 } while (k > 5);
 console.log(k);
+
+console.log("*** For loop output below ***");
+// for loop, use it when we know how many times we have to repeat loop
+for (z = 0; z <= 10; z++) {
+  console.log(z);
+}
+
+// 1. to find multiples of 2 and 5
+// 2. to find multiples of 2 or 5
+// 3. to find first 5 multiples of 5 and 10
+
+console.log("*** to find multiples of 2 and 5 output below ***");
+
+// using && to satisfy two scenarios
+// = is used to assign a value and == is used to equate a value as same or not
+for (a = 0; a <= 20; a++) {
+  if (a % 2 == 0 && a % 5 == 0) {
+    console.log(a);
+  }
+}
+
+console.log("*** to find multiples of 2 or 5 output below ***");
+
+// || operator to satsify either of the two values
+for (b = 0; b <= 20; b++) {
+  if (b % 2 == 0 || b % 5 == 0) {
+    console.log(b);
+  }
+}
+
+console.log("*** to find first 5 multiples of 5 and 10 output below ***");
+// using nested if block
+let d = 0;
+for (c = 1; c <= 50; c++) {
+  if (c % 5 == 0 && c % 10 == 0) {
+    console.log(c);
+    d++;
+    if (d == 5) {
+      break;
+    }
+  }
+}
